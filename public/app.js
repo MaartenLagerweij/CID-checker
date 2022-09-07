51f610ced204c327ec18576ce87d54c6db64fef3
Maarten Lagerweij <maarten.lagerweij1@gmail.com>
2022-09-07 15:59:14 +0200

    Polling functionality implemented

--- public/app.js
+++ public/app.js
@@ -1,41 +1,30 @@
-const getBtn = document.getElementById('get');
 const postBtn = document.getElementById('post');
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 
-// const eventSource = new EventSource('http://localhost:8383/sse/');
-
-// function updateMessage (message){
-//     const list = document.getElementById('messages');
-//     const item = document.createElement('p');
-//     item.textContent = message;
-//     list.appendChild(item);
-// }
-
-// eventSource.onmessage = function(event){
-//     updateMessage(event.data);
-// }
-
-// eventScource.onerror = function(){
-//     updateMessage('Server closed connection');
-//     eventSource.close();
-// }
-
 const baseURL = 'http://localhost:8383/info/';
 
-getBtn.addEventListener('click', getInfo);
 postBtn.addEventListener('click', postInfo);
 
-async function getInfo (e){
-    e.preventDefault();
-    const response = await fetch(baseURL).then(data => data.json());
-    let rows = '';
-        response.forEach(urlItem => {
-            const row = `<tr><td>${urlItem.url}</td><td>${urlItem.salesForceSyntaxURL}</td></tr>`;
-            rows+= row;
-        })
-        output.innerHTML = '';
-    output.insertAdjacentHTML('beforeend', `<table>${rows}</table>`);
+function polling(){
+    const lengthInputUrls = input.value.split(/\n/).length;
+
+    const intervalId = setInterval(async () => {
+
+        const response = await fetch(baseURL).then(data => data.json());
+
+        if(lengthInputUrls === response.length) {
+            let rows = '';
+            response.forEach(urlItem => {
+                const row = `<tr><td>${urlItem.url}</td><td>${urlItem.salesForceSyntaxURL}</td></tr>`;
+                rows+= row;
+            })
+            output.innerHTML = '';
+            output.insertAdjacentHTML('beforeend', `<table>${rows}</table>`);
+            clearInterval(intervalId)
+        };
+    }, 1000)
+
 }
 
 async function postInfo(e){
@@ -50,4 +39,6 @@ async function postInfo(e){
             urls: input.value
         })
     })
+    
+    polling();
 }
